a99a20c147855152c314d27db237fea143bff1de
MichelBundel <michel@bundel.eu>
2023-05-26 09:51:32 +0200

    hot fix

--- src/main/java/com/example/blogprojekt/BlogProjectApplication.java
+++ src/main/java/com/example/blogprojekt/BlogProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogProjektApplication {
+public class BlogProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogProjektApplication.class, args);
+		SpringApplication.run(BlogProjectApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/blogprojekt/BlogProjectApplicationTests.java
+++ src/test/java/com/example/blogprojekt/BlogProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BlogProjektApplicationTests {
+class BlogProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
