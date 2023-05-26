d755f524b2d3af5c044cdbd9b65d23ce147f689c
MichelBundel <michel@bundel.eu>
2023-05-26 09:48:12 +0200

    my first time commit

---
+++ src/main/java/com/example/blogprojekt/BlogProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogprojekt/BlogProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
