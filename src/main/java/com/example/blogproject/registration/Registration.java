f370ef737c766083a6fa91b63a1b6787c493ac01
JuliaWaller <julia.waller@brightstraining.com>
2023-06-01 00:17:38 +0200

    registration angelegt

---
+++ src/main/java/com/example/blogproject/registration/Registration.java
@@ -0,0 +1,31 @@
+package com.example.blogproject.registration;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Registration {
+
+    @Id
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String registrationName;
+
+    @Getter
+    @Setter
+    private String registrationPassword;
+
+    public Registration() {
+    }
+
+    public Registration(String registrationName, String registrationPassword) {
+        this.registrationName = registrationName;
+        this.registrationPassword = registrationPassword;
+    }
+}
